6a6cc57794b7c7f60e7dead9e72c37293c630721
Test cod <admin@sneppi.ru>
2022-11-17 04:47:46 +0300

    Изучение FrameLayout. Замена шаблона Activity_main на шаблон test_layout. Красный квадрат посередине.

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,3 +1,4 @@
+# Делает код нечитаемым, для релиза
 # Add project specific ProGuard rules here.
 # You can control the set of applied configuration files using the
 # proguardFiles setting in build.gradle.
--- app/src/main/java/com/example/myandroidtestgit/MainActivity.kt
+++ app/src/main/java/com/example/myandroidtestgit/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.myandroidtestgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {  //Создание MainActivity
+    override fun onCreate(savedInstanceState: Bundle?) {    //Метод onCreate
+        super.onCreate(savedInstanceState)  //Вызов метода из родительского класса AppCompatActivity
+        setContentView(R.layout.test_layout)  //todo Запуск макетного файла интерфейса при старте приложения
     }
 }
