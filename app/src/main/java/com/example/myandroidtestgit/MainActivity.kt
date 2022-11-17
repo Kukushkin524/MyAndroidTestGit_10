b576d33c889c3e8b83577df08d05bced4fc2d22e
Test cod <admin@sneppi.ru>
2022-11-17 05:20:53 +0300

    Макет(шаблон) в макете (шаблоне). Один внутри другого.

--- app/src/main/java/com/example/myandroidtestgit/MainActivity.kt
+++ app/src/main/java/com/example/myandroidtestgit/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {  //Создание MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {    //Метод onCreate
         super.onCreate(savedInstanceState)  //Вызов метода из родительского класса AppCompatActivity
-        setContentView(R.layout.test_layout)  //todo Запуск макетного файла интерфейса при старте приложения
+        setContentView(R.layout.activity_main)  //todo Запуск макетного файла интерфейса при старте приложения
     }
 }
